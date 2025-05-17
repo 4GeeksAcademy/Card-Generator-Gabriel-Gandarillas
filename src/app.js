1694e408d6c196e07668c8ff0c83e276b2f687b5
Gabriel Andrés Gandarillas Mendoza <gbr_360@hotmail.com>
2025-05-17 09:48:20 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,27 @@ import "bootstrap";
 import "./style.css";
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+
+window.onload = function generarcartas() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const palos = [
+    {icon: "bi-suit-heart-fill", color: "text-danger"},
+    {icon: "bi-suit-diamond-fill", color: "text-danger"},
+    {icon: "bi-suit-spade-fill", color: "text-dark"},
+    {icon: "bi-suit-club-fill", color: "text-dark"}
+  ]
+  const numeros =["2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K", "A"]
+  
+  let palo = palos[Math.floor(Math.random() * palos.length)]
+    
+  let numero = numeros[Math.floor(Math.random() * numeros.length)]
+    
+  document.getElementById("card-numero").textContent = numero
+
+  document.getElementById("card-palo-top").innerHTML = `<i class="bi ${palo.icon} ${palo.color}"></i>`
+
+  document.getElementById("card-palo-bottom").innerHTML = `<i class="bi ${palo.icon} ${palo.color}"></i>`
+
+
 };
